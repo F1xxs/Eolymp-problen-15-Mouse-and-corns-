acd381df5472a024f48afaaf616d252ac6b6ec69
Mike Oleskiv <149396128+F1xxs@users.noreply.github.com>
2023-10-30 16:17:22 +0200

    Update Mouse&Corns.py

--- mousedynamic.py
+++ mousedynamic.py
@@ -1,27 +1,15 @@
 m, n = map(int, input().split())
-# dim_list = [[float("inf")] * (n) for i in range(m)]
-# for i in range(m):
-#     for h, v in enumerate(map(int, input().split())):
-#         if dim_list[i][h] != float("inf"):
-#             continue
-#         dim_list[i][h] = v
 dim_list = []
 for _ in range(m):
     dim_list.append(list(map(int, input().split())))
-# print("dim_list", dim_list)
 dim_list = dim_list[::-1]
-# print(dim_list)
 for j in range(1, m):
     dim_list[j][0] = dim_list[j][0] + dim_list[j-1][0]
 for k in range(1, n):
     dim_list[0][k] = dim_list[0][k] + dim_list[0][k-1]
-# print("dim_list", dim_list)
 for g in range(1, m):
     for l in range(1, n):
         dim_list[g][l] = dim_list[g][l] + max(dim_list[g-1][l], dim_list[g][l-1])
-# print("dim_list", dim_list)
-# print("dim_list", dim_list)
-# print(dim_list[m-1][n-1])
 i = m - 1
 j = n - 1
 path = ""
@@ -39,4 +27,3 @@ while i or j:
         path += "F"
         i -= 1
 print(path[::-1])
-# while y < m and x < n:
